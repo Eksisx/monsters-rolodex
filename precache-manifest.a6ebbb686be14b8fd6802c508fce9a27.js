409c5244e84c1ff231afa012530bbce24f4851fc
Eksisx <canberk0494@gmail.com>
2019-09-03 13:15:41 +0300

    Updates

---
+++ precache-manifest.a6ebbb686be14b8fd6802c508fce9a27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fe89a1e358ed779f8652b9cbb98be2f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "046a34a9c41f02a670bf",
+    "url": "/monsters-rolodex/static/css/main.f6f49181.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "046a34a9c41f02a670bf",
+    "url": "/monsters-rolodex/static/js/main.2c786cc4.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
